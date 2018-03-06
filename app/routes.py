5ee4a6c035fb5ec4483965c41c6ac29056caa753
libraryman85 <libraryman85@gmail.com>
2018-03-06 12:50:55 -0500

    added index.html and templates

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
-def index():
+def index1():
     user = {'username': 'Alex'}
     return '''
     <html>
@@ -14,3 +16,9 @@ def index():
             <h1>Hello, ''' + user['username'] + '''!</h1>
         </body>
     </html>'''
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
