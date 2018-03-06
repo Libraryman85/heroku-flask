a88a031c517dac1c474c9a7dcac2583a4eb10d04
libraryman85 <libraryman85@gmail.com>
2018-03-06 12:46:22 -0500

    changed hello world to def index hello user

--- microblog.py
+++ microblog.py
@@ -6,16 +6,3 @@ manager = Manager(app)
 
 if __name__ == '__main__':
     manager.run()
-
-
-    def index():
-        user = {'username': 'Miguel'}
-        return '''
-    <html>
-        <head>
-            <title>Home Page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>'''
