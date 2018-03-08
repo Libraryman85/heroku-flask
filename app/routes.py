4e4d00cf0cdcdedd0774b99014a3437126465abb
libraryman85 <libraryman85@gmail.com>
2018-03-08 12:28:29 -0500

    adding loops

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,19 @@ from app import app
 @app.route('/index1')
 def index1():
     user = {'username': 'Alex'}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
     return '''
     <html>
         <head>
@@ -16,8 +29,9 @@ def index1():
         </body>
     </html>'''
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Alex'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Dashboard', adventurer=adventurer, games=games)
