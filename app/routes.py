ee47eca3b25e5591870a760f64769099d4406650
libraryman85 <libraryman85@gmail.com>
2018-03-08 12:38:14 -0500

    changed username

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Alex'}
     posts = [
         {
             'author': {'username': 'John'},
